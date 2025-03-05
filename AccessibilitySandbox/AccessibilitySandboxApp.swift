b7fbdcded5dc1689d1cd8a0f656f421dc76ed4d3
shashankb-cc <shashank.b@codecraft.co.in>
2025-03-05 11:54:27 +0530

    Initial Commit

---
+++ AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by Shashank B on 05/03/25.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
