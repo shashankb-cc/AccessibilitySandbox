65b14c9e3ad5ac37b076dccdd42de24247c4e705
shashankb-cc <shashank.b@codecraft.co.in>
2025-03-05 12:58:20 +0530

    added accessibilty label and hididng some not so imp data from accessibility system

--- AccessibilitySandbox/ContentView.swift
+++ AccessibilitySandbox/ContentView.swift
@@ -8,14 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    let pictures = [
+        "ales-krivec-15949",
+        "galina-n-189483",
+        "kevin-horstmann-141705",
+        "nicolas-tissot-335096"
+    ]
+    
+    let labels = [
+        "Tulips",
+        "Frozen tree buds",
+        "Sunflowers",
+        "Fireworks",
+    ]
+
+    @State private var selectedPicture = Int.random(in: 0...3)
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ScrollView {
+            
+            //using image and addinf on tap gesture works but bad for the accessibilty
+            //        Image(pictures[selectedPicture])
+            //            .resizable()
+            //            .scaledToFit()
+            //            .onTapGesture {
+            //                selectedPicture = Int.random(in: 0...3)
+            //            }
+            //            .accessibilityLabel(labels[selectedPicture])
+            //            .accessibilityAddTraits(.isButton)
+            
+            
+            //you can use button and label instead
+            Button {
+                selectedPicture = Int.random(in: 0...3)
+            } label: {
+                Image(pictures[selectedPicture])
+                    .resizable()
+                    .scaledToFit()
+            }
+            .accessibilityLabel(labels[selectedPicture])
+            
+            
+            //if image is not imp for accessibility
+            Image(decorative: "character")
+            
+            //        OR
+            Image(.character)
+                .accessibilityHidden(true)
+            
+            VStack {
+                Text("Your score is")
+                Text("1000")
+                    .font(.title)
+            }
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel("Your score is 1000")
         }
-        .padding()
     }
 }
 
